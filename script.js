fd64ee51cfabd9ca5986a425aed7afb57bb0ac32
Kania Watkins <144135880+kwatkins25@users.noreply.github.com>
2024-04-04 14:21:34 +0000

    array5

--- script.js
+++ script.js
@@ -4,15 +4,27 @@
 
 // 1. Create a variable named mood.
 //  - Assign it a string value of "happy".
-
+let mood = "happy";
 
 // 2. Create a variable named recommendedSong. Do not assign a value.
-
+let recommendedSong;
 
 // 3. Implement the recommendation logic.
 // - If the mood is equal to "happy", recommend "Here Comes the Sun by The Beatles".
 // - If the mood is equal to "sad", recommend "Someone Like You by Adele".
 // - Anything else, recommend "Break My Soul by Beyonce".
+
+
+if (mood === "happy" ) {
+        console.log("Here Comes the Sun by The Beatles");
+  } 
+  
+else if (mood === "sad" ){
+        console.log("Someone Like You by Adele");
+    }
+ else {
+        console.log("Break My Soul by Beyonce");
+ }
 
 
 // 4. Console log the recommended song.
